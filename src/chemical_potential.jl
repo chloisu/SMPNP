56a043b061891861d01e4de3c26a6c616d6123ce
Niklaus M. Leuenberger <nik@DN56q2qj.SUNet>
2025-03-14 16:11:34 -0700

    implement change in flux law from Sidian

--- src/chemical_potential.jl
+++ src/chemical_potential.jl
@@ -22,7 +22,7 @@ function μ(c1, c2, z1, ϕ, parameters::Parameters)
             m2 = parameters.species_parameters.ion_sizes[ANION_EQ-1]^3 * MOL_PER_LITER_TO_PER_CUBIC_METER
         end
         sum = 1 - c1 * m1 - c2 * m2
-        μ1 = m1 / m0 * (rlog(c1 * m1) - rlog(sum)) + z1 * ϕ
+        μ1 = (rlog(c1 * m1) - m1 / m0 * rlog(sum)) + z1 * ϕ
     end
     return μ1
 end
@@ -39,7 +39,7 @@ function μ_due_to_diff(c1, c2, z1, ϕ, parameters::Parameters)
             m2 = parameters.species_parameters.ion_sizes[ANION_EQ-1]^3 * MOL_PER_LITER_TO_PER_CUBIC_METER
         end
         sum = 1 - c1 * m1 - c2 * m2
-        μ1 = m1 / m0 * (rlog(c1 * m1))
+        μ1 = rlog(c1 * m1)
     end
     return μ1
 end
